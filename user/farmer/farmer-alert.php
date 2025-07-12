8cb5efd93d762e363e3b4aa9079f4c782d0814f7
Your Name <you@example.com>
2025-07-12 21:21:46 +0800

    heh

--- user/farmer/farmer-alert.php
+++ user/farmer/farmer-alert.php
@@ -1,10 +1,14 @@
 <?php 
 
-if ($page_title=="Index") {
-	if(isset($_SESSION['is_farm_registered']) && $_SESSION['is_farm_registered']==0){
-		echo "<div class='farm-alert-message'>Your farm details is not set, please set the details <a href='farm_detail.php'>HERE</a></div>";
+if ($page_title == "Index") {
+	if (isset($_SESSION['is_farm_registered']) && $_SESSION['is_farm_registered'] == 0) {
+		echo "
+		<div class='farm-alert'>
+			<strong>Notice:</strong> Your farm details are not set. Please complete them <a href='farm_detail.php'>here</a>.
+		</div>";
 	}
 }
+
 
 
 
