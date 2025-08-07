24523dddacd3add1c02606f0abfe156547b9bd88
Your Name <you@example.com>
2025-08-07 16:49:24 +0800

    sql

--- sql/harvesthub.sql
+++ sql/harvesthub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2025 at 10:52 PM
+-- Generation Time: Aug 07, 2025 at 10:49 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -38,21 +38,49 @@ CREATE TABLE `categories` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `farm_details`
+--
+
+CREATE TABLE `farm_details` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `municipality` varchar(250) NOT NULL,
+  `baranggay` varchar(250) NOT NULL,
+  `purok` varchar(255) NOT NULL,
+  `farm_ownership` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `modified_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL,
-  `price` int(15) NOT NULL,
+  `product_name` varchar(250) NOT NULL,
+  `price_per_unit` int(20) NOT NULL,
   `category` varchar(255) NOT NULL,
-  `seller_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `description` varchar(300) NOT NULL,
-  `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `modified` timestamp NOT NULL DEFAULT current_timestamp()
+  `product_image` varchar(255) NOT NULL,
+  `product_description` varchar(300) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `modified` timestamp NOT NULL DEFAULT current_timestamp(),
+  `unit` varchar(20) NOT NULL,
+  `total_stocks` int(50) NOT NULL,
+  `lot_size` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_name`, `price_per_unit`, `category`, `user_id`, `quantity`, `product_image`, `product_description`, `created_at`, `modified`, `unit`, `total_stocks`, `lot_size`) VALUES
+(12, 'Sitaw', 20, 'Vegetable', 2, 0, '28b90ed444694a47cfe87a27c67a4e279c538f1b-qweqweqw.png', 'Great', '2025-08-07 15:45:45', '2025-08-07 07:45:45', 'kilos', 0, '20'),
+(13, 'kangkong', 20, 'Vegetable', 2, 0, '28b90ed444694a47cfe87a27c67a4e279c538f1b-123123.png', 'great', '2025-08-07 15:47:16', '2025-08-07 07:47:16', 'ton', 0, '20');
 
 -- --------------------------------------------------------
 
@@ -73,16 +101,16 @@ CREATE TABLE `users` (
   `rating` int(10) NOT NULL,
   `created` datetime NOT NULL DEFAULT current_timestamp(),
   `modified` datetime NOT NULL DEFAULT current_timestamp(),
-  `first_time_logged_in` int(11) NOT NULL
+  `first_time_logged_in` int(11) NOT NULL,
+  `farm_details_exists` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `baranggay`, `address`, `user_type`, `email_address`, `contact_number`, `password`, `rating`, `created`, `modified`, `first_time_logged_in`) VALUES
-(1, 'evelyn', 'gascon', 'boac', '', 'consumer', '', '09533307696', '', 0, '2025-03-10 05:44:40', '2025-03-10 05:44:40', 3),
-(2, 'evelyn', 'gascon', 'boac', '', 'consumer', '', '09533307696', '', 0, '2025-03-10 05:49:38', '2025-03-10 05:49:38', 0);
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `baranggay`, `address`, `user_type`, `email_address`, `contact_number`, `password`, `rating`, `created`, `modified`, `first_time_logged_in`, `farm_details_exists`) VALUES
+(2, 'evelyn', 'gascon', 'boac', '', 'Farmer', '', '09533307696', '', 0, '2025-03-10 05:49:38', '2025-03-10 05:49:38', 0, 1);
 
 --
 -- Indexes for dumped tables
@@ -92,6 +120,12 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `baranggay`, `address`, `use
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `farm_details`
+--
+ALTER TABLE `farm_details`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -117,10 +151,16 @@ ALTER TABLE `categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `farm_details`
+--
+ALTER TABLE `farm_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
