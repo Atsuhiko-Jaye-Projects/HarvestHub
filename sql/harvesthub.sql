7fe584f6e960bd5a508516de8ca6f1146a5396b7
Your Name <you@example.com>
2025-08-08 19:04:59 +0800

    Hrvst_Hub-51636

--- sql/harvesthub.sql
+++ sql/harvesthub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 07, 2025 at 10:49 AM
+-- Generation Time: Aug 08, 2025 at 01:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -55,6 +55,69 @@ CREATE TABLE `farm_details` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `farm_products`
+--
+
+CREATE TABLE `farm_products` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_name` varchar(250) NOT NULL,
+  `date_planted` varchar(250) NOT NULL,
+  `estimated_harvest_date` varchar(255) NOT NULL,
+  `yield` int(11) NOT NULL,
+  `suggested_price` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `modified_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `farm_products`
+--
+
+INSERT INTO `farm_products` (`id`, `user_id`, `product_name`, `date_planted`, `estimated_harvest_date`, `yield`, `suggested_price`, `created_at`, `modified_at`) VALUES
+(1, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:51:31', '2025-08-08 10:51:31'),
+(2, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:51:34', '2025-08-08 10:51:34'),
+(3, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:51:51', '2025-08-08 10:51:51'),
+(4, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:52:12', '2025-08-08 10:52:12'),
+(5, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:52:30', '2025-08-08 10:52:30'),
+(6, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:52:39', '2025-08-08 10:52:39'),
+(7, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:52:55', '2025-08-08 10:52:55'),
+(8, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:53:06', '2025-08-08 10:53:06'),
+(9, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:53:12', '2025-08-08 10:53:12'),
+(10, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:53:13', '2025-08-08 10:53:13'),
+(11, 2, 'Sitaw', '2025-08-05', '2025-08-23', 5, 24, '2025-08-08 18:53:46', '2025-08-08 10:53:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `farm_resources`
+--
+
+CREATE TABLE `farm_resources` (
+  `id` int(11) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `item_name` varchar(50) NOT NULL,
+  `cost` int(10) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `modified_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `farm_resources`
+--
+
+INSERT INTO `farm_resources` (`id`, `user_id`, `item_name`, `cost`, `date`, `created_at`, `modified_at`, `type`) VALUES
+(1, 2, 'Kubota', 5000, '2025-08-05', '2025-08-08 18:10:02', '2025-08-08 10:10:02', ''),
+(2, 2, 'Kubota', 5000, '2025-08-05', '2025-08-08 18:10:30', '2025-08-08 10:10:30', ''),
+(3, 2, 'Kubota', 5000, '2025-08-06', '2025-08-08 18:14:20', '2025-08-08 10:14:20', ''),
+(4, 2, 'Kubota', 5000, '2025-08-13', '2025-08-08 18:15:21', '2025-08-08 10:15:21', ''),
+(5, 2, 'Kubota', 5000, '2025-08-13', '2025-08-08 18:15:58', '2025-08-08 10:15:58', 'Machine');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -80,7 +143,10 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `product_name`, `price_per_unit`, `category`, `user_id`, `quantity`, `product_image`, `product_description`, `created_at`, `modified`, `unit`, `total_stocks`, `lot_size`) VALUES
 (12, 'Sitaw', 20, 'Vegetable', 2, 0, '28b90ed444694a47cfe87a27c67a4e279c538f1b-qweqweqw.png', 'Great', '2025-08-07 15:45:45', '2025-08-07 07:45:45', 'kilos', 0, '20'),
-(13, 'kangkong', 20, 'Vegetable', 2, 0, '28b90ed444694a47cfe87a27c67a4e279c538f1b-123123.png', 'great', '2025-08-07 15:47:16', '2025-08-07 07:47:16', 'ton', 0, '20');
+(13, 'kangkong', 20, 'Vegetable', 2, 0, '28b90ed444694a47cfe87a27c67a4e279c538f1b-123123.png', 'great', '2025-08-07 15:47:16', '2025-08-07 07:47:16', 'ton', 0, '20'),
+(14, 'Sitaw', 20, 'Vegetable', 2, 0, '28b90ed444694a47cfe87a27c67a4e279c538f1b-qweqwewq.png', 'great', '2025-08-07 16:55:34', '2025-08-07 08:55:34', 'kilos', 0, '20'),
+(15, 'Sitaw', 20, 'Vegetable', 2, 0, '28b90ed444694a47cfe87a27c67a4e279c538f1b-qweqwewq.png', 'great', '2025-08-07 16:55:46', '2025-08-07 08:55:46', 'kilos', 0, '20'),
+(16, 'Sitaw', 20, 'Vegetable', 2, 0, '28b90ed444694a47cfe87a27c67a4e279c538f1b-qweqwewq.png', 'great', '2025-08-08 13:33:56', '2025-08-08 05:33:56', 'kilos', 0, '20');
 
 -- --------------------------------------------------------
 
@@ -129,6 +195,18 @@ ALTER TABLE `farm_details`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `farm_products`
+--
+ALTER TABLE `farm_products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `farm_resources`
+--
+ALTER TABLE `farm_resources`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -157,10 +235,22 @@ ALTER TABLE `farm_details`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
+-- AUTO_INCREMENT for table `farm_products`
+--
+ALTER TABLE `farm_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `farm_resources`
+--
+ALTER TABLE `farm_resources`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
