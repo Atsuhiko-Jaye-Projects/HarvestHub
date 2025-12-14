4efcb9f097449b5030e0ecd42c1c4033addd9103
Your Name <you@example.com>
2025-12-14 14:34:57 +0800

    sql

--- sql/harvesthub.sql
+++ sql/harvesthub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2025 at 06:42 AM
+-- Generation Time: Dec 14, 2025 at 07:34 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
